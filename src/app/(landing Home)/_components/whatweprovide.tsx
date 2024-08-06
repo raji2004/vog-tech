589c7d0fa5ce0d17092bac4297bdb1b3af99d28b
raji <kingraj1344@gmail.com>
2024-08-06 12:31:12 +0100

    done

---
+++ src/app/(landing Home)/_components/whatweprovide.tsx
@@ -0,0 +1,117 @@
+'use client';
+import { H1, H2, P, List } from "@/components/typography"
+import { IconButton } from "@/components/button"
+import { ArrowRight, ArrowLeft } from "lucide-react"
+import { useRef } from "react"
+
+import Image from "next/image"
+
+export const WhatWeProvide = () => {
+    return (
+        <div className="p-10 bg-primary w-full text-left space-y-7">
+            <div className=" md:flex md:justify-center">
+
+                <H2 className=" font-normal " color={"text-primary-foreground"}>
+                    We provide the best financial services for your business
+                </H2>
+                <P color={"text-primary-foreground"}>
+                    Successfully audited and consulted for over 500 businesses across various industries.
+                </P>
+            </div>
+
+            <Carosel />
+            <div>
+                <P color={"text-primary-foreground"}>Here at VOG Tech:</P>
+                <List color={"text-primary-foreground"}>
+                    <li>We are utilizing the latest technologies and methodologies to ensure compliance and efficiency.</li>
+                    <li>Ensuring your business meets all Nigerian tax laws and regulations.</li>
+                    <li>Recognized by leading industry bodies and certified by [relevant authority].</li>
+                    <li>Clear and transparent processes with regular updates and reports.</li>
+                </List>
+            </div>
+        </div>
+    )
+}
+
+
+const Carosel = () => {
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const imgSize = 300
+    const scroll = (direction: "left" | "right") => {
+        if (scrollRef.current) {
+            const { scrollLeft, clientWidth } = scrollRef.current;
+            const scrollAmount = direction === "left" ? -clientWidth : clientWidth;
+            scrollRef.current.scrollTo({ left: scrollLeft + scrollAmount, behavior: "smooth" });
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-3">
+            <div className="flex self-end gap-3">
+                <IconButton
+                    onClick={() => scroll("left")}
+                    className="bg-primary text-primary-foreground border border-primary-foreground"
+                >
+                    <ArrowLeft size={24} />
+                </IconButton>
+                <IconButton
+                    onClick={() => scroll("right")}
+                    className="bg-primary text-primary-foreground border border-primary-foreground"
+                >
+                    <ArrowRight size={24} />
+                </IconButton>
+            </div>
+            <div ref={scrollRef} className="overflow-x-scroll flex gap-3 no-scrollbar">
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+                <Image
+                    src="/img/home/whatweprovide.svg"
+                    width={imgSize}
+                    height={imgSize}
+                    className="rounded-lg"
+                    alt="what we provide"
+                />
+            </div>
+        </div>
+    );
+};
--- src/app/(landing Home)/page.tsx
+++ src/app/(landing Home)/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Hero } from "./_components/hero";
+import { WhatWeProvide } from "./_components/whatweprovide";
 
 export default function Home() {
   return (
-    <main className=" p-10 ">
-    <Hero/>
+    <main className=" ">
+      <Hero />
+      <WhatWeProvide />
     </main>
   );
 }
