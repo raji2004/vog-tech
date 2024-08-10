08b2e3bf0504ca6ffc144750f9ead9cb45e9e00d
raji <kingraj1344@gmail.com>
2024-08-10 17:12:30 +0100

    chore: Update footer component with additional content and styling
    The footer component in the landing page has been updated to include additional content and styling changes. The spacing between sections has been increased to improve readability and visual appeal. The footer now includes a section for displaying the VOG Global logo and a brief description of the company's services. Two separate lists have been added to provide information about the services offered by VOG Global and its different divisions. Lastly, a subscription form has been included to allow users to subscribe to newsletters. These updates enhance the overall user experience and provide more comprehensive information about VOG Global.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export const Footer = () => {
     return (
-        <div className=" bg-white p-4 space-y-7 flex flex-col items-center">
+        <div className=" bg-white p-4 space-y-36 flex flex-col items-center">
             <div className=" bg-primary w-11/12 md:w-3/4 md:flex md:flex-col md:items-center text-center rounded-3xl p-10 space-y-5">
                 <H3 color="text-primary-foreground">Ready To Change Your Business</H3>
                 <P className=" text-[10px] md:text-sm" color="text-primary-foreground">
@@ -25,6 +25,46 @@ export const Footer = () => {
                         Get Started
                     </Button>
                 </div>
+            </div>
+
+            <div className=" flex flex-col md:flex-row gap-10 items-center justify-evenly">
+                <div className=" flex flex-col md:w-1/3 text-left gap-4">
+
+                    <Image
+                        src="/icon/logo.svg"
+                        alt="logo"
+                        width={100}
+                        height={100}
+                        className=" justify-self-start md:justify-self-center"
+                    />
+                    <P className=" text-[10px] md:text-sm" color="text-secondary-foreground">
+                        VOG Global is a leading provider of consulting, tax audit, and assurance services.
+                        Our team of seasoned professionals is dedicated to providing tailored solutions that drive growth, enhance efficiency, and ensure compliance.
+                    </P>
+                </div>
+
+                <List className=" text-secondary-foreground  text-center  list-none">
+                    <H3 color="text-secondary-foreground" className=" font-bold"> Our Services</H3>
+                    <li>IT Audits</li>
+                    <li>Internal Auditing</li>
+                    <li>External Auditing</li>
+                    <li>Financial Reviews</li>
+                    <li>Financial Services</li>
+                    <li>Tax Auditing Services</li>
+                </List>
+                <List className=" text-secondary-foreground  text-center  list-none">
+                    <H3 color="text-secondary-foreground" className=" font-bold"> VOG Global</H3>
+                    <li>VOG Finance </li>
+                    <li>VOG GlobalTech</li>
+                    <li>VOG Global Consult</li>
+                </List>
+
+                <div className=" flex flex-col items-center md:items-start gap-4">
+                    <H3 color="text-secondary-foreground">Subscribe To Our Newsletters</H3>
+                    <Input placeholder=" Enter your email" noLabel className=" border text-secondary-foreground" />
+                    <Button className=" border border-secondary-foreground bg-white text-secondary-foreground"> Suscribe</Button>
+                </div>
+
 
             </div>
 
